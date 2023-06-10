625f19ce64b554e275bf0ddcc9aa478dfc1179a0
Ivan Melnychenko <ivanmelnychenko@buildops.com>
2023-06-10 08:10:19 -0400

    uncomment main class

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-//        Item coke = new Item("drink", "coke", 1.50);
-//        coke.printItem();
-//        coke.setSize("LARGE");
-//        coke.printItem();
-//
-//        Item avocado = new Item("Topping", "avocado", 1.50);
-//        avocado.printItem();
+        Item coke = new Item("drink", "coke", 1.50);
+        coke.printItem();
+        coke.setSize("LARGE");
+        coke.printItem();
 
-//        Burger burger = new Burger("regular", 4.00);
-//        burger.addToppings("BACON", "CHEESE", "MAYO");
-//        burger.printItem();
+        Item avocado = new Item("Topping", "avocado", 1.50);
+        avocado.printItem();
 
-//        MealOrder regularMeal = new MealOrder();
-//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
-//        regularMeal.setDrinkSize("LARGE");
-//        regularMeal.printItemizedList();
+        Burger burger = new Burger("regular", 4.00);
+        burger.addToppings("BACON", "CHEESE", "MAYO");
+        burger.printItem();
+
+        MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();
 
         MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "cili");
         deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
